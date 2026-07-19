d3fd5f1bab63e53d3268f7578d4237c50d395093
RanjithKumar94 <ranjithkumar3593@gmail.com>
2026-07-19 21:15:06 +0530

    radar.js

--- js/radar.js
+++ js/radar.js
@@ -148,11 +148,6 @@ function drawCentreline(){
 
 function drawTrafficCircuit(){
 
-    alert("NEW TRAFFIC CIRCUIT");
-
-
-}
-{
 
     const end08 = bearingToXY(260,12);
     const end26 = bearingToXY(80,12);
