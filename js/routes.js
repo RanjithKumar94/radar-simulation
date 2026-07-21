9eeaa450c6b47e65de2d3bc87e525c1ebc425b26
RanjithKumar94 <ranjithkumar3593@gmail.com>
2026-07-21 09:32:00 +0530

    routes.js

---
+++ js/routes.js
@@ -0,0 +1,34 @@
+const routes = [
+
+{
+ name:"CCB-PJ",
+ points:[
+   {
+    radial:190,
+    distance:30
+   }
+ ]
+},
+
+
+{
+ name:"PJ160",
+ start:"PJ",
+ heading:160
+},
+
+
+{
+ name:"PJ109",
+ start:"PJ",
+ heading:109
+},
+
+
+{
+ name:"CCB-NAG",
+ start:"CCB",
+ heading:88
+}
+
+];
