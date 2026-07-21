f5359d32ee3502fab038c1c50c4c04e941a002d3
RanjithKumar94 <ranjithkumar3593@gmail.com>
2026-07-21 23:17:47 +0530

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,10 @@
 console.log("routes.js loaded");
 
 
-function drawRoutes(){
+// ========================
+// Route data
+// ========================
 
-    console.log("drawing routes");
-
-}
 const routes = [
 
 {
@@ -18,20 +17,17 @@ const routes = [
  ]
 },
 
-
 {
  name:"PJ160",
  start:"PJ",
  heading:160
 },
 
-
 {
  name:"PJ109",
  start:"PJ",
  heading:109
 },
-
 
 {
  name:"CCB-NAG",
@@ -40,3 +36,52 @@ const routes = [
 }
 
 ];
+
+
+// ========================
+// Draw routes on radar
+// ========================
+
+function drawRoutes(){
+
+    console.log("drawing routes");
+
+
+    ctx.strokeStyle="#555555";
+    ctx.lineWidth=1;
+
+
+    // CCB to PJ
+
+    const PJ = bearingToXY(190,30);
+
+
+    ctx.beginPath();
+
+    ctx.moveTo(
+        CCB.x,
+        CCB.y
+    );
+
+
+    ctx.lineTo(
+        PJ.x,
+        PJ.y
+    );
+
+
+    ctx.stroke();
+
+
+    // PJ label
+
+    ctx.fillStyle="#FFFFFF";
+    ctx.font="12px Consolas";
+
+    ctx.fillText(
+        "PJ",
+        PJ.x+5,
+        PJ.y
+    );
+
+}
